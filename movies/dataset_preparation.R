169981b954d34c8f73ec52bdc9901e74038edb9b
JRuedas <11670560+JRuedas@users.noreply.github.com>
2021-01-05 15:47:49 +0100

    Fixes aux functions to save list and changes heatmap

--- movies/dataset_preparation.R
+++ movies/dataset_preparation.R
@@ -3,10 +3,14 @@ library("ggplot2")
 library("dplyr")
 
 # Aux functions
+get_names <- function(col) {
+  lapply(fromJSON(col), function(genre) genre$name)
+}
+
 parseJSON <- function(column) {
   result <- NULL
   for (i in 1:length(column)) {
-    result[i] <- as.data.frame(fromJSON(column[i])[1])$name
+    result[i] <- list(get_names(column[i]))
   }
   result
 }
@@ -19,14 +23,17 @@ as_decade <- function(year) {
 movies <- read.csv(file="data/tmdb_5000_movies.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 
 # Removes useless columns
-keep <- c("budget", "genres", "production_countries", "revenue", "runtime", "vote_average", "title", "release_date")
+keep <- c("budget", "genres", "keywords", "production_companies", "production_countries", "revenue", "runtime", "vote_average", "title", "release_date")
 dataset <- movies[keep]
 
 # Removes missing values
+dataset <- dataset[!duplicated(dataset$title), ]
 dataset <- dataset[dataset$budget != 0,]
 dataset <- dataset[dataset$revenue != 0,]
 dataset <- dataset[dataset$genres != "[]",]
 dataset <- dataset[dataset$production_countries != "[]",]
+dataset <- dataset[dataset$keywords != "[]",]
+dataset <- dataset[dataset$production_companies != "[]",]
 
 # Parse date to new columns
 dataset$release_date <- as.Date(dataset$release_date)
@@ -39,7 +46,10 @@ dataset$earnings <- dataset$revenue - dataset$budget
 
 # Parse JSON columns
 dataset$genres <- parseJSON(dataset$genres)
+dataset$keywords <- parseJSON(dataset$keywords)
+dataset$production_companies <- parseJSON(dataset$production_companies)
 dataset$production_countries <- parseJSON(dataset$production_countries)
+
 
 # Create decade column
 dataset$decade <- factor(sapply(as.numeric(dataset$year), as_decade))
@@ -59,14 +69,19 @@ earnings_by_decade <- dataset %>%
                           group_by(year, decade) %>%
                           summarise(total = sum(earnings))
 
-ggplot(data = earnings_by_year, mapping = aes(x = earnings_by_year$year, y = earnings_by_year$month, fill = earnings_by_year$total)) +
-  geom_tile() +
-  scale_fill_gradient(low="white", high="red") +
+ggplot(earnings_by_year, aes(earnings_by_year$year, as.factor(earnings_by_year$month))) +
+  geom_tile(aes(fill = earnings_by_year$total), colour="white") +
+  scale_fill_gradient(low="light green", high="dark green") +
   labs(x = "Years", y = "Months", fill = "Earnings") +
   theme_light()
 
-ggplot(data = earnings_by_decade, aes(x = earnings_by_decade$decade, y = earnings_by_decade$year)) +
-  geom_tile(aes(fill = earnings_by_decade$total)) +
-  scale_fill_gradient(low="white", high="red") +
+ggplot(earnings_by_decade, aes(earnings_by_decade$decade, as.factor(earnings_by_decade$year))) +
+  geom_tile(aes(fill = earnings_by_decade$total), colour="white") +
+  scale_fill_gradient(low="light green", high="dark green") +
   labs(x = "Decades", y = "Years", fill = "Earnings") +
   theme_light()
+
+
+# Pregunta 3.
+
+# 
