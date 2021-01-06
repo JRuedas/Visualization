888ba82a5760b836980a136e07c30e99086dfa9f
Jose Dominguez Perez <jose.dperez@alumnos.upm.es>
2021-01-06 13:49:41 +0100

    Almost Finished

--- movies/app.R
+++ movies/app.R
@@ -10,9 +10,7 @@
 ## from CRAN and then loaded.
 
 ## First specify the packages of interest
-packages = c("shiny")
-
-movies <- read.csv(file="data/cleaned_tmdb_5000_movies.csv", header=TRUE, sep=",")
+packages = c("shiny", "ggplot2", "dplyr", "wordcloud")
 
 ## Now load or install & load all
 package.check <- lapply(
@@ -25,25 +23,181 @@ package.check <- lapply(
   }
 )
 
+# Load data
+dataset <- read.csv(file="data/cleaned_tmdb_5000_movies.csv", header=TRUE, sep=",")
+
+companies <- sort(unique(dataset$production_companies))
+companies <- c("Choose a company", companies)
+
+countries <- sort(unique(dataset$production_countries))
+
+# Aux Functions
+as_decade <- function(year) {
+  year - (year %% 10)
+}
+
 # User interface ----
 ui <- fluidPage(
   titlePanel("Big data: TMDB Movies project"),
   
   sidebarLayout(
     sidebarPanel(
-      
-      
       br(),
       br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      selectInput("selected_decade", 
+                  label = "Choose a decade to display",
+                  choices = seq(1970, 2010, 10),
+                  selected = 1970),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      hr(style = "border-top: 1px solid black;"),
+      br(),
+      br(),
+      selectInput("selected_company1", 
+                  label = "Company 1",
+                  choices = companies,
+                  selected = 'Paramount Pictures'),
+      selectInput("selected_company2", 
+                  label = "Company 2",
+                  choices = companies,
+                  selected = 'Walt Disney Pictures'),
+      selectInput("selected_company3", 
+                  label = "Company 3",
+                  choices = companies,
+                  selected = 'Warner Bros.'),
+      selectInput("selected_company4", 
+                  label = "Company 4",
+                  choices = companies,
+                  selected = companies[1]),
+      selectInput("selected_company5", 
+                  label = "Company 5",
+                  choices = companies,
+                  selected = companies[1]),
+      hr(style = "border-top: 1px solid black;"),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      selectInput("selected_country", 
+                  label = "Choose a country to display",
+                  choices = countries,
+                  selected = "United States of America"),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br(),
+      br()
     ),
     
-    mainPanel()
+    mainPanel(
+      h4("First Question", align = "center"),
+      plotOutput("heat_map"),
+      hr(style = "border-top: 1px solid black;"),
+      h4("Second Question", align = "center"),
+      plotOutput("line_char"),
+      hr(style = "border-top: 1px solid black;"),
+      plotOutput("wordcloud"),
+      br(),
+      br(),
+      br()
+    )
   )
 )
 
 # Server logic
 server <- function(input, output) {
-
+  
+  ################# FIRST QUESTION #################
+  
+  output$heat_map <- renderPlot({
+    selected_decade <- input$selected_decade
+    
+    # Aggregate earnings by year and month from selected decade
+    earnings_by_year <- dataset %>%
+      filter(decade == selected_decade) %>%
+      group_by(month, year) %>%
+      summarise(total = mean(earnings)) %>%
+      arrange(year, month)
+    
+    ggplot(earnings_by_year, aes(earnings_by_year$year, as.factor(earnings_by_year$month))) +
+      geom_tile(aes(fill = earnings_by_year$total), colour="white") +
+      scale_fill_gradient(low="light green", high="dark green") +
+      labs(x = "Years", y = "Months", fill = "Earnings") +
+      theme_light()
+  })
+  
+  # Note: negative profits are shown as 0 earnings
+  
+  # Several conclusions:
+  # 1) Most beneficial in summer, maybe because people have more time to go to cinema
+  # 2) Moreover, earnings are increasing over time
+  
+  ############### END FIRST QUESTION ###############
+  
+  ############### SECOND QUESTION ###############
+  
+  output$line_char <- renderPlot({
+    
+    selected_companies = c(
+      input$selected_company1,
+      input$selected_company2,
+      input$selected_company3,
+      input$selected_company4,
+      input$selected_company5
+    )
+    
+    rating_by_year <- dataset %>%
+      filter(production_companies %in% selected_companies) %>%
+      group_by(year, production_companies) %>%
+      summarise(total = sum(vote_average))
+    
+    ggplot(rating_by_year, aes(rating_by_year$year, rating_by_year$total,
+                               group=unlist(rating_by_year$production_companies),
+                               color=unlist(rating_by_year$production_companies))) +
+      geom_line() +
+      labs(x = "Year", y = "Average") +
+      scale_color_discrete(name="Companies") +
+      theme_light()
+  })
+  
+  ############# END SECOND QUESTION #############
+  
+  ############### THIRD QUESTION ###############
+  
+  output$wordcloud <- renderPlot({
+    
+    genres_by_year <- dataset %>%
+      filter(production_countries %in% input$selected_country) %>%
+      group_by(production_countries,genres) %>%
+      count(genres)
+    
+    set.seed(1234)
+    wordcloud(words = genres_by_year$genres, freq = genres_by_year$n, min.freq = 1,
+              max.words=200, random.order=FALSE, rot.per=0.35, 
+              colors=brewer.pal(8, "Dark2"))
+  })
+  ############# END THIRD QUESTION #############
 }
 
 # Run the app
--- movies/dataset_preparation.R
+++ movies/dataset_preparation.R
@@ -1,18 +1,27 @@
-library("rjson")
-library("ggplot2")
-library("dplyr")
+## First specify the packages of interest
+packages = c("rjson", "ggplot2", "dplyr", "wordcloud")
+
+## Now load or install & load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+######################## DATA PREPARATION ######################## 
 
 # Aux functions
-get_names <- function(col) {
-  lapply(fromJSON(col), function(genre) genre$name)
+get_name <- function(col) {
+  y <- sapply(fromJSON(col), function(x) x$name)
+  y[1]
 }
 
 parseJSON <- function(column) {
-  result <- NULL
-  for (i in 1:length(column)) {
-    result[i] <- list(get_names(column[i]))
-  }
-  result
+  sapply(column, get_name, USE.NAMES = FALSE) # Do not use column as name
 }
 
 as_decade <- function(year) {
@@ -50,24 +59,24 @@ dataset$keywords <- parseJSON(dataset$keywords)
 dataset$production_companies <- parseJSON(dataset$production_companies)
 dataset$production_countries <- parseJSON(dataset$production_countries)
 
-
 # Create decade column
 dataset$decade <- factor(sapply(as.numeric(dataset$year), as_decade))
 
 # Saves CSV
 write.csv(x = dataset, file="data/cleaned_tmdb_5000_movies.csv")
 
-dataset <- dataset[as.numeric(dataset$year) >= 1990 & as.numeric(dataset$year) <= 2019,]
+###################### END DATA PREPARATION ######################
 
-# Aggregate earnings by year and month
+################# FIRST QUESTION #################
+
+# Show from 1970-1979 to 2010-2019 -> 5 decades
+
+# Aggregate earnings by year and month from selected decade
 earnings_by_year <- dataset %>%
+                      filter(decade == 2010) %>%
                       group_by(month, year) %>%
-                      summarise(total = sum(earnings))
-
-# Aggregate earnings by decade and month
-earnings_by_decade <- dataset %>%
-                          group_by(year, decade) %>%
-                          summarise(total = sum(earnings))
+                      summarise(total = mean(earnings)) %>%
+                      arrange(year, month)
 
 ggplot(earnings_by_year, aes(earnings_by_year$year, as.factor(earnings_by_year$month))) +
   geom_tile(aes(fill = earnings_by_year$total), colour="white") +
@@ -75,13 +84,46 @@ ggplot(earnings_by_year, aes(earnings_by_year$year, as.factor(earnings_by_year$m
   labs(x = "Years", y = "Months", fill = "Earnings") +
   theme_light()
 
-ggplot(earnings_by_decade, aes(earnings_by_decade$decade, as.factor(earnings_by_decade$year))) +
-  geom_tile(aes(fill = earnings_by_decade$total), colour="white") +
-  scale_fill_gradient(low="light green", high="dark green") +
-  labs(x = "Decades", y = "Years", fill = "Earnings") +
+# Note: negative profits are shown as 0 earnings
+
+# Several conclusions:
+# 1) Most beneficial in summer, maybe because people have more time to go to cinema
+# 2) Moreover, earnings are increasing over time
+
+############### END FIRST QUESTION ###############
+
+############### SECOND QUESTION ###############
+
+a <- c('Paramount Pictures','Marvel Studios','DC Comics','Warner Bros.','Walt Disney Pictures') 
+rating_by_year <- dataset %>%
+  filter(production_companies %in% a) %>%
+  group_by(year, production_companies) %>%
+  summarise(total = mean(vote_average))
+
+ggplot(rating_by_year, aes(rating_by_year$year, rating_by_year$total,group=unlist(rating_by_year$production_companies),
+                           color=c("red","black","orange","yellow","blue"))) +
+  geom_line() +
+  labs(x = "Year", y = "Average") +
+  scale_color_discrete(name="Companies") +
+  scale_x_discrete(breaks = seq(min(as_decade(as.numeric(rating_by_year$year))),
+                                max(as_decade(as.numeric(rating_by_year$year))),
+                                10)) +
   theme_light()
 
+############# END SECOND QUESTION #############
 
-# Pregunta 3.
+############### THIRD QUESTION ###############
 
-# 
+b <- c('United States of America')
+
+genres_by_year <- dataset %>%
+  filter(production_countries %in% b) %>%
+  group_by(production_countries,genres) %>%
+  count(genres)
+
+set.seed(1234)
+wordcloud(words = genres_by_year$genres, freq = genres_by_year$n, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+############# END THIRD QUESTION #############
