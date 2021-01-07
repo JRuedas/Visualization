cc86db039db135a351667d723df42baf6cab5f1a
JRuedas <11670560+JRuedas@users.noreply.github.com>
2021-01-07 13:58:29 +0100

    Changes way of using library because deployment fails

--- movies/app.R
+++ movies/app.R
@@ -4,24 +4,9 @@
 # Jonatan Ruedas Mora
 
 # Libraries
-
-## If a package is installed, it will be loaded. If any
-## are not, the missing package(s) will be installed
-## from CRAN and then loaded.
-
-## First specify the packages of interest
-packages = c("shiny", "ggplot2", "dplyr")
-
-## Now load or install & load all
-package.check <- lapply(
-  packages,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-)
+library(shiny)
+library(ggplot2)
+library(dplyr)
 
 # Load data
 dataset <- read.csv(file="data/cleaned_tmdb_5000_movies.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
